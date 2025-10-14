3a1c84560eb197d1eb5aea13db80fb18324ca3cd
hernandezb3 <brittney.hernandez@uconn.edu>
2025-10-14 13:08:21 -0400

    test docker container

---
+++ resources/docker.sh
@@ -0,0 +1,17 @@
+# build and run a Docker container from the a Docker image:
+# instead of using Apptainer to build the container
+
+# login to docker
+docker login
+
+# pull the latest ollama image
+docker pull ollama/ollama:latest # docker
+
+# run the container
+# -d = run container in background
+# -v = bind mount volume
+# -p = publish containers ports to the host
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:latest
+
+# run download and run llama3.2 3B
+docker exec -it ollama ollama pull llama3.2:3b
